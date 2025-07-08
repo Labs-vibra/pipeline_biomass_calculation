f7f4032fd2a8378e24f910ccaff7cb0e611047dc
dwbessa <danielbessa01@gmail.com>
2025-07-08 16:03:01 -0300

    fix: schemas

--- src/db/schemas/raw/ddl_total_sales.sql
+++ src/db/schemas/raw/ddl_total_sales.sql
@@ -1,8 +1,6 @@
 CREATE TABLE IF NOT EXISTS rw_ext_anp.venda_total (
     veto_dat_venda DATE,
     veto_txt_razao_social STRING,
-    veto_txt_base_cnpj STRING,
-    veto_txt_nome_produtor STRING,
     veto_qtd_volume_1000m3 FLOAT64,
     veto_txt_produto STRING,
     veto_txt_origem_regiao STRING,
