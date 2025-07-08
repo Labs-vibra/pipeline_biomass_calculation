0c957d987e69cc2d789a04dc65bba0fb908d70d8
dwbessa <danielbessa01@gmail.com>
2025-07-08 16:02:18 -0300

    feat: trusted layer complete

--- src/db/queries/trusted/dml_total_sales.sql
+++ src/db/queries/trusted/dml_total_sales.sql
@@ -1,25 +1,62 @@
-CREATE OR REPLACE TABLE td_ext_biomass_calculation.td_ext_anp_total_sales
-PARTITION BY DATE(date)
-AS
-SELECT
-	DATE(date) AS date,
-	company_name,
-  	product,
-  	CAST(REPLACE(volume_1000m3, ',', '.') AS FLOAT64) AS volume_1000m3,
-  	target_market,
-  	destination_state,
-  	origin_region,
-  	origin_state,
-  	destination_region,
-  	product_name,
-  	EXTRACT(MONTH FROM DATE(date)) AS month
-FROM
-	rw_ext_biomass_calculation.rw_ext_anp_total_sales
-WHERE
-  	product LIKE '%DIESEL B%'
-  	AND NOT (
-    	LOWER(product) LIKE '%diesel b20%'
-    	OR LOWER(product) LIKE '%diesel b15%'
-    	OR LOWER(product) LIKE '%diesel b2%'
-  	)
-  	AND date >= {{start_date}} AND date <= {{end_date}}
+MERGE td_ext_anp.venda_total AS target
+USING (
+  SELECT
+    veto_dat_venda,
+    veto_txt_razao_social,
+    veto_txt_produto,
+    CAST(REPLACE(veto_qtd_volume_1000m3, ',', '.') AS FLOAT64) AS veto_qtd_volume_1000m3,
+    veto_txt_mercado_destino,
+    veto_txt_destino_estado,
+	veto_txt_origem_regiao,
+    veto_txt_origem_estado,
+	veto_txt_destino_regiao,
+    veto_txt_nome_produto,
+    veto_txt_codigo_produto,
+    EXTRACT(MONTH FROM DATE(veto_dat_venda)) AS month
+  FROM
+    rw_ext_anp.venda_total
+  WHERE
+    veto_txt_produto LIKE '%DIESEL B%'
+    AND NOT (
+      LOWER(veto_txt_produto) LIKE '%diesel b20%'
+      OR LOWER(veto_txt_produto) LIKE '%diesel b15%'
+      OR LOWER(veto_txt_produto) LIKE '%diesel b2%'
+    )
+    AND veto_dat_venda >= {{start_date}} AND veto_dat_venda <= {{end_date}}
+) AS source
+ON target.veto_dat_venda = source.veto_dat_venda 
+   AND target.veto_txt_razao_social = source.veto_txt_razao_social 
+   AND target.veto_txt_produto = source.veto_txt_produto
+   AND target.veto_txt_mercado_destino = source.veto_txt_mercado_destino
+   AND target.veto_txt_destino_estado = source.veto_txt_destino_estado
+   AND target.veto_txt_origem_estado = source.veto_txt_origem_estado
+WHEN MATCHED THEN
+  UPDATE SET
+    veto_qtd_volume_1000m3 = source.veto_qtd_volume_1000m3
+WHEN NOT MATCHED THEN
+  INSERT (
+    veto_dat_venda,
+    veto_txt_razao_social,
+    veto_txt_produto,
+    veto_qtd_volume_1000m3,
+    veto_txt_mercado_destino,
+    veto_txt_destino_estado,
+    veto_txt_origem_regiao,
+    veto_txt_origem_estado,
+    veto_txt_destino_regiao,
+    veto_txt_nome_produto,
+    veto_txt_codigo_produto
+  )
+  VALUES (
+    source.veto_dat_venda,
+    source.veto_txt_razao_social,
+    source.veto_txt_produto,
+    source.veto_qtd_volume_1000m3,
+    source.veto_txt_mercado_destino,
+    source.veto_txt_destino_estado,
+    source.veto_txt_origem_regiao,
+    source.veto_txt_origem_estado,
+    source.veto_txt_destino_regiao,
+    source.veto_txt_nome_produto,
+    CAST(source.veto_txt_codigo_produto AS STRING)
+  );
