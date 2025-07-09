ec477545788b048bc8015308ae8a5013633b3a9e
leeticiamm <leeticiam@hotmail.com>
2025-07-09 14:49:58 -0300

    create sql congeneres sales - trusted

---
+++ src/db/queries/trusted/dml_congeneres_sales.sql
@@ -0,0 +1,44 @@
+MERGE td_ext_anp.venda_congeneres AS target
+USING (
+  SELECT
+    veco_dat_venda,
+    veco_txt_produto,
+    veco_txt_origem,
+    veco_txt_destino,
+    veco_txt_vendedor,
+    veco_txt_comprador,
+    veco_qtd_volume_1000m3,
+  FROM
+    rw_ext_anp.venda_congeneres
+  WHERE
+    LOWER(veco_txt_produto) LIKE '%diesel b%'
+    AND veco_dat_venda BETWEEN {{start_date}} AND {{end_date}}
+) AS source
+ON target.veco_dat_venda = source.veco_dat_venda 
+   AND target.veco_nom_produto = source.veco_txt_produto
+   AND target.veco_txt_origem = source.veco_txt_origem
+   AND target.veco_txt_destino = source.veco_txt_destino
+   AND target.veco_nom_vendedor = source.veco_txt_vendedor
+   AND target.veco_nom_comprador = source.veco_txt_comprador
+WHEN MATCHED THEN
+  UPDATE SET
+    veco_qtd_volume_1000m3 = source.veco_qtd_volume_1000m3
+WHEN NOT MATCHED THEN
+  INSERT (
+    veco_dat_venda,
+    veco_nom_produto,
+    veco_txt_origem,
+    veco_txt_destino,
+    veco_nom_vendedor,
+    veco_nom_comprador,
+    veco_qtd_volume_1000m3
+  )
+  VALUES (
+    veco_dat_venda,
+    veco_txt_produto,
+    veco_txt_origem,
+    veco_txt_destino,
+    veco_txt_vendedor,
+    veco_txt_comprador,
+    veco_qtd_volume_1000m3
+  );
