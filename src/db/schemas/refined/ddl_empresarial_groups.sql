f2069fc4c968fb4824c5df671100f024c746ae29
fenol64 <fenol64.dev@gmail.com>
2025-07-14 15:37:49 -0300

    varchar to string

--- src/db/schemas/refined/ddl_empresarial_groups.sql
+++ src/db/schemas/refined/ddl_empresarial_groups.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS rf_ext_anp.empresarial_groups (
-    ge_nom_agente_regulado VARCHAR(255) NOT NULL,
-    ge_nom_empresa VARCHAR(255) NOT NULL,
-    ge_txt_cluster VARCHAR(255) NOT NULL
+    ge_nom_agente_regulado STRING NOT NULL,
+    ge_nom_empresa STRING NOT NULL,
+    ge_txt_cluster STRING NOT NULL
 );
