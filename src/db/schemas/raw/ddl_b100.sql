301f1844acec9cf17accd0791ec766f84b90f2f0
fenol64 <fenol64.dev@gmail.com>
2025-07-16 16:32:57 -0300

    v1

--- src/db/schemas/raw/ddl_b100.sql
+++ src/db/schemas/raw/ddl_b100.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS rw_ext_anp.venda_b100 (
-    vb100_dat_compra DATE,
-    vb100_num_cnpj STRING,
-    vb100_txt_razao_social STRING,
-    vb100_qtd_volume FLOAT64,
-    vb100_nom_produtor STRING,
-    vb100_num_produtor_cnpj STRING,
-    vb100_dat_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
-) PARTITION BY DATE(vb100_dat_criacao);
+    data_compra DATE,
+    raiz_cnpj_distribuidor STRING,
+    razao_social_distribuidor STRING,
+    qtd_de_produto_m3 FLOAT64,
+    razao_social_produtor STRING,
+    cnpj_produtor STRING,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+) PARTITION BY DATE(data_criacao);
--- src/db/schemas/raw/ddl_congeres.sql
+++ src/db/schemas/raw/ddl_congeres.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS rw_ext_anp.venda_congeneres (
-    veco_dat_venda DATE,
-    veco_txt_produto STRING,
-    veco_txt_origem STRING,
-    veco_txt_destino STRING,
-    veco_txt_vendedor STRING,
-    veco_txt_comprador STRING,
-    veco_qtd_volume_1000m3 FLOAT64,
-    veco_dat_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
-) PARTITION BY DATE(veco_dat_criacao);
+    periodo DATE,
+    produto STRING,
+    uf_origem STRING,
+    uf_destino STRING,
+    vendedor STRING,
+    comprador STRING,
+    qtd_produto_liquido FLOAT64,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+) PARTITION BY DATE(data_criacao);
