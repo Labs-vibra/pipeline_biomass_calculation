901326b8c76b1f5e70c23b1eaf73c793e8737e55
dwbessa <danielbessa01@gmail.com>
2025-07-16 15:54:18 -0300

    fix: new field data_upload

--- src/db/schemas/raw/ddl_dados_agentes.sql
+++ src/db/schemas/raw/ddl_dados_agentes.sql
@@ -10,5 +10,6 @@ CREATE TABLE IF NOT EXISTS rw_ext_anp.dados_agentes (
     tipo_do_agente STRING,
     data_inicio_validade INTEGER,
     data_final_validade FLOAT64,
-    data_versao DATE
+    data_versao DATE,
+    data_upload TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 );
--- src/db/schemas/refined/ddl_dados_agentes.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS rf_ext_anp.dados_agentes (
-    agnt_num_base_cnpj STRING NOT NULL,
-    agnt_nom_razao_social STRING NOT NULL
-);
