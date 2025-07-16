fb81f589e545a5dbc029887626b8ea00b14e1f51
dwbessa <danielbessa01@gmail.com>
2025-07-16 12:36:14 -0300

    feat: now creating a dictionary in BQ and inserting it in refined layer

--- src/db/queries/refined/dml_biomass_calculation.sql
+++ src/db/queries/refined/dml_biomass_calculation.sql
@@ -100,91 +100,100 @@ dieselb_compra_congenere AS (
 ),
 
 todas_datas_empresas AS (
-	SELECT DISTINCT
-		dat,
-		razao_social,
-	FROM (
-    SELECT dat, razao_social FROM venda_b100
-    UNION DISTINCT
-    SELECT dat, razao_social FROM venda_total
-    UNION DISTINCT
-    SELECT dat, razao_social FROM b100_venda_congenere
-    UNION DISTINCT
-    SELECT dat, razao_social FROM b100_compra_congenere
-    UNION DISTINCT
-    SELECT dat, razao_social FROM dieselb_venda_congenere
-    UNION DISTINCT
-    SELECT dat, razao_social FROM dieselb_compra_congenere
-  )
+    SELECT DISTINCT
+        dat,
+        razao_social,
+        cnpj.agnt_num_base_cnpj
+    FROM (
+        SELECT dat, razao_social FROM venda_b100
+        UNION DISTINCT
+        SELECT dat, razao_social FROM venda_total
+        UNION DISTINCT
+        SELECT dat, razao_social FROM b100_venda_congenere
+        UNION DISTINCT
+        SELECT dat, razao_social FROM b100_compra_congenere
+        UNION DISTINCT
+        SELECT dat, razao_social FROM dieselb_venda_congenere
+        UNION DISTINCT
+        SELECT dat, razao_social FROM dieselb_compra_congenere
+    ) empresas
+    LEFT JOIN rf_ext_anp.cnpj_agentes cnpj 
+        ON empresas.razao_social = cnpj.agnt_nom_razao_social
 ),
 
 dados_consolidados AS (
-	SELECT
-		tde.dat,
-		tde.razao_social,
-		COALESCE(vb.volume_1000m3, 0) AS b100_venda_volume_1000m3,
-		COALESCE(vt.volume_1000m3, 0) AS venda_total_volume_1000m3,
-		COALESCE(bvc.volume_1000m3, 0) AS b100_venda_congenere_volume_1000m3,
-		COALESCE(bcc.volume_1000m3, 0) AS b100_compra_congenere_volume_1000m3,
-		COALESCE(dvc.volume_1000m3, 0) AS dieselb_venda_congenere_volume_1000m3,
-		COALESCE(dcc.volume_1000m3, 0) AS dieselb_compra_congenere_volume_1000m3
-	FROM
-		todas_datas_empresas tde
-	LEFT JOIN venda_b100 vb ON tde.dat = vb.dat AND tde.razao_social = vb.razao_social
-	LEFT JOIN venda_total vt ON tde.dat = vt.dat AND tde.razao_social = vt.razao_social
-	LEFT JOIN b100_venda_congenere bvc ON tde.dat = bvc.dat AND tde.razao_social = bvc.razao_social
-	LEFT JOIN b100_compra_congenere bcc ON tde.dat = bcc.dat AND tde.razao_social = bcc.razao_social
-	LEFT JOIN dieselb_venda_congenere dvc ON tde.dat = dvc.dat AND tde.razao_social = dvc.razao_social
-	LEFT JOIN dieselb_compra_congenere dcc ON tde.dat = dcc.dat AND tde.razao_social = dcc.razao_social
+    SELECT
+        tde.dat,
+        tde.razao_social,
+        tde.agnt_num_base_cnpj,
+        COALESCE(vb.volume_1000m3, 0) AS b100_venda_volume_1000m3,
+        COALESCE(vt.volume_1000m3, 0) AS venda_total_volume_1000m3,
+        COALESCE(bvc.volume_1000m3, 0) AS b100_venda_congenere_volume_1000m3,
+        COALESCE(bcc.volume_1000m3, 0) AS b100_compra_congenere_volume_1000m3,
+        COALESCE(dvc.volume_1000m3, 0) AS dieselb_venda_congenere_volume_1000m3,
+        COALESCE(dcc.volume_1000m3, 0) AS dieselb_compra_congenere_volume_1000m3
+    FROM
+        todas_datas_empresas tde
+    LEFT JOIN venda_b100 vb ON tde.dat = vb.dat AND tde.razao_social = vb.razao_social
+    LEFT JOIN venda_total vt ON tde.dat = vt.dat AND tde.razao_social = vt.razao_social
+    LEFT JOIN b100_venda_congenere bvc ON tde.dat = bvc.dat AND tde.razao_social = bvc.razao_social
+    LEFT JOIN b100_compra_congenere bcc ON tde.dat = bcc.dat AND tde.razao_social = bcc.razao_social
+    LEFT JOIN dieselb_venda_congenere dvc ON tde.dat = dvc.dat AND tde.razao_social = dvc.razao_social
+    LEFT JOIN dieselb_compra_congenere dcc ON tde.dat = dcc.dat AND tde.razao_social = dcc.razao_social
 ),
 
 necessidade_teorica_b100 AS (
-	SELECT
-	dat,
-	razao_social,
-	(venda_total_volume_1000m3 + dieselb_compra_congenere_volume_1000m3 - dieselb_venda_congenere_volume_1000m3) * 
-    CASE 
-      WHEN EXTRACT(YEAR FROM dat) = 2023 AND EXTRACT(MONTH FROM dat) <= 3 
-        THEN MANDATORY_BIODIESEL_UNTIL_MAR_23
-      WHEN EXTRACT(YEAR FROM dat) = 2023 AND EXTRACT(MONTH FROM dat) > 3 
-        THEN MANDATORY_BIODIESEL_UNTIL_FEB_24
-      WHEN EXTRACT(YEAR FROM dat) = 2024 AND EXTRACT(MONTH FROM dat) <= 2 
-        THEN MANDATORY_BIODIESEL_UNTIL_FEB_24
-      WHEN EXTRACT(YEAR FROM dat) = 2024 AND EXTRACT(MONTH FROM dat) > 2 
-        THEN MANDATORY_BIODIESEL_UNTIL_DEC_25
-      ELSE MANDATORY_BIODIESEL_UNTIL_DEC_25
-    END AS necessidade_teorica_b100,
-    
-  	FROM
-		dados_consolidados
+    SELECT
+        dat,
+        razao_social,
+        agnt_num_base_cnpj,
+        (venda_total_volume_1000m3 + dieselb_compra_congenere_volume_1000m3 - dieselb_venda_congenere_volume_1000m3) * 
+        CASE 
+            WHEN EXTRACT(YEAR FROM dat) = 2023 AND EXTRACT(MONTH FROM dat) <= 3 
+                THEN MANDATORY_BIODIESEL_UNTIL_MAR_23
+            WHEN EXTRACT(YEAR FROM dat) = 2023 AND EXTRACT(MONTH FROM dat) > 3 
+                THEN MANDATORY_BIODIESEL_UNTIL_FEB_24
+            WHEN EXTRACT(YEAR FROM dat) = 2024 AND EXTRACT(MONTH FROM dat) <= 2 
+                THEN MANDATORY_BIODIESEL_UNTIL_FEB_24
+            WHEN EXTRACT(YEAR FROM dat) = 2024 AND EXTRACT(MONTH FROM dat) > 2 
+                THEN MANDATORY_BIODIESEL_UNTIL_DEC_25
+            ELSE MANDATORY_BIODIESEL_UNTIL_DEC_25
+        END AS necessidade_teorica_b100
+    FROM
+        dados_consolidados
 ),
 
 compra_liquida_b100 AS (
-	SELECT
-		dat,
-		razao_social,
-		(b100_venda_volume_1000m3 + b100_compra_congenere_volume_1000m3 - b100_venda_congenere_volume_1000m3) AS compra_liquida_b100
-	FROM
-		dados_consolidados
+    SELECT
+        dat,
+        razao_social,
+        agnt_num_base_cnpj,
+        (b100_venda_volume_1000m3 + b100_compra_congenere_volume_1000m3 - b100_venda_congenere_volume_1000m3) AS compra_liquida_b100
+    FROM
+        dados_consolidados
 )
 
 SELECT
-	clb.dat AS cabi_dat_calculo,
-	clb.razao_social AS cabi_txt_razao_social,
-	clb.compra_liquida_b100 AS cabi_qtd_compra_liq_b100,
-	ntb.necessidade_teorica_b100 AS cabi_qtd_necessidade_teorica_b100,
-	(ntb.necessidade_teorica_b100 - clb.compra_liquida_b100) AS cabi_qtd_dif_biodiesel,
-	0 AS cabi_qtd_estoque,
-	(ntb.necessidade_teorica_b100 - clb.compra_liquida_b100) + 0 AS cabi_qtd_gap_liq_b100
+    clb.dat AS cabi_dat_calculo,
+    clb.razao_social AS cabi_txt_razao_social,
+    clb.agnt_num_base_cnpj AS cabi_num_base_cnpj,
+    clb.compra_liquida_b100 AS cabi_qtd_compra_liq_b100,
+    ntb.necessidade_teorica_b100 AS cabi_qtd_necessidade_teorica_b100,
+    (ntb.necessidade_teorica_b100 - clb.compra_liquida_b100) AS cabi_qtd_dif_biodiesel,
+    0 AS cabi_qtd_estoque,
+    (ntb.necessidade_teorica_b100 - clb.compra_liquida_b100) + 0 AS cabi_qtd_gap_liq_b100
 FROM 
-	compra_liquida_b100 clb
+    compra_liquida_b100 clb
 JOIN
-	necessidade_teorica_b100 ntb ON clb.dat = ntb.dat AND clb.razao_social = ntb.razao_social
+    necessidade_teorica_b100 ntb ON clb.dat = ntb.dat 
+        AND clb.razao_social = ntb.razao_social 
+        AND clb.agnt_num_base_cnpj = ntb.agnt_num_base_cnpj
 ) AS source
 ON target.cabi_dat_calculo = source.cabi_dat_calculo 
    AND target.cabi_txt_razao_social = source.cabi_txt_razao_social
 WHEN MATCHED THEN
   UPDATE SET
+    cabi_num_base_cnpj = source.cabi_num_base_cnpj,
     cabi_qtd_compra_liq_b100 = source.cabi_qtd_compra_liq_b100,
     cabi_qtd_necessidade_teorica_b100 = source.cabi_qtd_necessidade_teorica_b100,
     cabi_qtd_dif_biodiesel = source.cabi_qtd_dif_biodiesel,
@@ -194,6 +203,7 @@ WHEN NOT MATCHED THEN
   INSERT (
     cabi_dat_calculo,
     cabi_txt_razao_social,
+    cabi_num_base_cnpj,
     cabi_qtd_compra_liq_b100,
     cabi_qtd_necessidade_teorica_b100,
     cabi_qtd_dif_biodiesel,
@@ -201,8 +211,9 @@ WHEN NOT MATCHED THEN
     cabi_qtd_gap_liq_b100
   )
   VALUES (
-	source.cabi_dat_calculo,
+    source.cabi_dat_calculo,
     source.cabi_txt_razao_social,
+    source.cabi_num_base_cnpj,
     source.cabi_qtd_compra_liq_b100,
     source.cabi_qtd_necessidade_teorica_b100,
     source.cabi_qtd_dif_biodiesel,
---
+++ src/db/schemas/refined/ddl_cnpj_agentes.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS rf_ext_anp.cnpj_agentes (
+    agnt_num_base_cnpj STRING NOT NULL,
+    agnt_nom_razao_social STRING NOT NULL
+);
